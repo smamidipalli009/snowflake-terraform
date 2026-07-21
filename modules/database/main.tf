e5f56dccea53e1d80b706560cc940b6d32ae7e86
smamidipalli009 <mamidipalli009@gmail.com>
2026-07-20 22:33:00 -0400

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -4,16 +4,6 @@
 # tags, and comment metadata. Designed for enterprise multi-environment use.
 ##############################################################################
 
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    snowflake = {
-      source  = "Snowflake-Labs/snowflake"
-      version = "~> 0.90"
-    }
-  }
-}
 
 ##############################################################################
 # Database
